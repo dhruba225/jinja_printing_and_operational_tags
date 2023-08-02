3561c3c70a0158811cc5c75c7c0922a50ca5f26b
dhruba225 <dhru225@gmail.com>
2023-08-02 20:07:54 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def data_render(request):
     d={'Name':'Dhruba','Age':23}
     return render(request,'data_render.html',context=d)
+
+def condition(request):
+    dict={'A':10,'B':20}
+    return render(request,'condition.html',context=dict)

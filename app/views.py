147e886216f48cba793dc93ba904b95922700761
dhruba225 <dhru225@gmail.com>
2023-08-03 19:58:10 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -6,5 +6,5 @@ def data_render(request):
     return render(request,'data_render.html',context=d)
 
 def condition(request):
-    dict={'A':100,'B':20,'C':150}
+    dict={'A':10,'B':20,'C':15}
     return render(request,'condition.html',context=dict)
